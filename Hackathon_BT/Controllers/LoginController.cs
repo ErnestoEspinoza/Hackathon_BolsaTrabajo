73b3bd67979c99d5c677a0bcd839860479146c90
Fernando Baldenebro <fernandofigueroa95@hotmail.com>
2017-10-12 17:20:09 -0600

    Ejemplo login Controllers

---
+++ Hackathon_BT/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Hackathon.STR;
+using Hackathon.DAO;
+
+namespace Hackathon_BT.Controllers
+{
+    public class LoginController
+    {
+        [RoutePrefix("api/Usuarios")]
+        public class DatosController : ApiController
+        {
+            [Route("Login")]
+            public string Get(string usuario, string contra)
+            {
+                DAOUsuarios datos = new DAOUsuarios();
+                return datos.Login(usuario, contra);
+            }
+        }
+    }
+}
