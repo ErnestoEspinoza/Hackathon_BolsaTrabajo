695cf64a38e1eac9493e60b6045e615451d7aec3
Fernando Baldenebro <fernandofigueroa95@hotmail.com>
2017-10-12 22:52:25 -0600

    Correccion DAO y STR .. comentarop en DAOUsuarios

---
+++ Hackathon.STR/strUsuarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.STR
+{
+   public class strUsuarios
+    {
+        int iD;
+        public int ID
+        {
+            get { return iD; }
+            set { iD = value; }
+        }
+        string usuario;
+        public string Usuario
+        {
+            get {return usuario; }
+            set { usuario = value; }
+        }
+        string contra;
+        public string Contra
+        {
+            get { return contra; }
+            set { contra = value; }
+        }
+        string tipoUsuario;
+        public string TipoUsuario
+        {
+            get { return tipoUsuario; }
+            set { tipoUsuario = value; }
+        }
+        string estatus;
+        public string Estatus
+        {
+            get { return estatus; }
+            set { estatus = value; }
+        }
+    }
+}
