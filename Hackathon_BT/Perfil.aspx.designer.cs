93bc27b3d97f4858f556fd18175038fc690dabb7
cesar-soto96@hotmail.com <cesar-soto96@hotmail.com>
2017-10-15 00:41:57 -0600

    Agregando perfil de empleado

---
+++ Hackathon_BT/Perfil.aspx.designer.cs
@@ -0,0 +1,15 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Hackathon_BT {
+    
+    
+    public partial class Perfil {
+    }
+}
